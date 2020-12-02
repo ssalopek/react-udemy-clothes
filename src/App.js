8f51b4779ec0d6efe872627df4841132c9fde451
ssalopek <ssalopek.hr@gmail.com>
2020-12-02 19:24:19 +0100

    added header component, links to hope, shop page and contact

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
