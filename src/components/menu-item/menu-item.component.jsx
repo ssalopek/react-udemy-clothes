2d8c0199500a99d110ad7510d87132ebeb4a84f7
ssalopek <ssalopek.hr@gmail.com>
2020-12-02 16:59:03 +0100

    update menu-items to navigate withRouter

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+/*withRouter will pass updated match, location, 
+and history props to the wrapped component whenever it renders*/
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+/*HISTORY -> library lets you easily manage session history anywhere JavaScript runs 
+  MATCH -> object contains information about how a <Route path> matched the URL*/
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{
@@ -13,10 +21,9 @@ const MenuItem = ({ title, imageUrl, size }) => (
     />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
-
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
