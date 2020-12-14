f8b58cc6eb94729bdc5ed9668a24e377064c6ede
ssalopek <ssalopek.hr@gmail.com>
2020-12-14 18:11:10 +0100

    added redux-persist to save local data after refreshing the page

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+/* 
+PersistGate delays the rendering of your app's UI until 
+your persisted state has been retrieved and saved to redux
+*/
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 import App from "./App";
 
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
